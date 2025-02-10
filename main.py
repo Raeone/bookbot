0918f40ef97b873ac64bcf8c572f56e0dbe38d06
Raeone <pay124@seznam.cz>
2025-02-10 22:15:08 +0100

    edit main: count words

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def main():
-  frankenstein_path = "books/frankenstein.txt"
-  print(get_file_content(frankenstein_path))
+  book_path = "books/frankenstein.txt"
+  book_content = get_file_content(book_path)
+  # print(book_content)
+  print(count_words(book_content))
 
 def get_file_content(path):
   with open(path) as file:
     return file.read()
+
+def count_words(text):
+  text_arr = text.split()
+  return len(text_arr)
+
+
 
 
 
