6529c7c6b8f526eaeab348c254112c93f65fa3f6
Raeone <pay124@seznam.cz>
2025-03-01 16:49:44 +0100

    print report

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from stats import count_words
 from stats import count_chars 
+from stats import sorted_chars
 
 def main():
   frankenstein_path = "books/frankenstein.txt"
   frankenstein_content = get_book_text(frankenstein_path)
+  chars_dict = count_chars(frankenstein_content)
+  sorted_characters = sorted_chars(chars_dict)
 
   # print(frankenstein_content)
-  print(f"{count_words(frankenstein_content)} words found in the document")
-  print(count_chars(frankenstein_content))
+  # print(f"{count_words(frankenstein_content)} words found in the document")
+  # print(count_chars(frankenstein_content))
+  # print(sorted_characters)
+
+  print("============ BOOKBOT ============")
+  print(f"Analyzing book found at {frankenstein_path}...")
+  print("----------- Word Count ----------")
+  print(f"Found {count_words(frankenstein_content)} total words")
+  print("--------- Character Count -------")
+  
+  for char in sorted_characters:
+    for key in char:
+      if key.isalpha():
+        print(f"{key}: {char[key]}")
+
+  print("============= END ===============")
+
+
+
   
 def get_book_text(file_to_path):
   with open(file_to_path) as f:
--- stats.py
+++ stats.py
@@ -13,3 +13,17 @@ def count_chars(text):
       char_count[char] = 1
   
   return char_count
+
+def sort_on(dict):
+  for key in dict:
+    return(dict[key])
+  
+def sorted_chars(dictionary_of_chars):
+  chars_arr = []
+
+  for key in dictionary_of_chars:
+    chars_arr.append({key: dictionary_of_chars[key] })
+
+  chars_arr.sort(key=sort_on, reverse=True)
+  
+  return chars_arr
