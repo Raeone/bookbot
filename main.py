51b45be1b3a97600dc015587c38e83d03447d488
Raeone <pay124@seznam.cz>
2025-03-01 14:33:42 +0100

    refactor

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 def main():
   frankenstein_path = "books/frankenstein.txt"
-  frankenstain_content = get_book_text(frankenstein_path)
+  frankenstein_content = get_book_text(frankenstein_path)
 
-  # print(frankenstain_content)
-  print(f"{count_words(frankenstain_content)} words found in the document")
+  # print(frankenstein_content)
+  print(f"{get_num_words(frankenstein_content)} words found in the document")
   
 def get_book_text(file_to_path):
   with open(file_to_path) as f:
     return f.read()
 
-def count_words(text):
+def get_num_words(text):
   text_array = text.split()
   return len(text_array)
   
